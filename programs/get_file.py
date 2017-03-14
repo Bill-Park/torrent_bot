fe37a39aa1fabde2f709b075b2262e3bc879b0ae
Bill-Park <tkddn0916@gmail.com>
2017-03-14 15:04:09 +0000

    base of torrent

--- programs/get_file.py
+++ programs/get_file.py
@@ -1,2 +1,27 @@
 from telegram.ext import Updater, MessageHandler, Filters
+import os
+from bill import login_data
+
+dir_now = os.path.dirname(os.path.abspath(__file__))
+dir_home = os.path.join(dir_now, '..')
+dir_torrent = os.path.join(dir_home, 'torrent_wait')
+
+print('get file start')
+print('dir is : ' + dir_now)
+
+def get_file(bot, update) :
+    file_id_short = update.message.document.file_id
+    file_url = os.path.join(dir_torrent, update.message.document.file_name)
+    bot.getFile(file_id_short).download(file_url)
+    update.message.reply_text('file saved')
+    print(update.message.document.file_name)
+
+
+updater = Updater(login_data.token)
+
+file_handler = MessageHandler(Filters.document, get_file)
+updater.dispatcher.add_handler(file_handler)
+
+updater.start_polling(timeout=3, clean=True)
+updater.idle()
 
--- programs/rpc.py
+++ programs/rpc.py
@@ -5,12 +5,17 @@ from bill import login_data
 
 dir_now = os.path.dirname(os.path.abspath(__file__))
 dir_home = os.path.join(dir_now, '..')
+dir_torrent = os.path.join(dir_home, 'torrent_wait')
 
 print('home : ' + dir_now)
 
 t_client = transmissionrpc.Client('localhost', port=9091, user=login_data.user, password=login_data.passwd)
 
-
+for t_file in os.listdir(dir_torrent) :
+    t_path = os.path.join(dir_torrent, t_file)
+    t_client.add_torrent(t_path)
+    print(t_client.get_torrents())
+    #print(t_file)
 
 '''
 t_main = t_client.get_torrent(0)
